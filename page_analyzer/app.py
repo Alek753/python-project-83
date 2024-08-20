71cb4f8522208bf7ec40373b79468442e244ec59
Aleksei Semenov <alek753@yandex.ru>
2024-08-20 16:01:13 +0400

    added db create script

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,11 +1,15 @@
 import os
 from flask import Flask, render_template
 from dotenv import load_dotenv
+import psycopg2
 
 
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
+
+conn = psycopg2.connect(app.config['DATABASE_URL'])
 
 
 @app.route('/')
