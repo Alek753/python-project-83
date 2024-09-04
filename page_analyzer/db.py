e9766e9164fc0feca26b3147fb8f7a32c8b9f4ca
Aleksei Semenov <alek753@yandex.ru>
2024-09-04 22:13:22 +0400

    refactoring db module using decorators

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -7,73 +7,67 @@ load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 
-def db_connect():
-    return connect(DATABASE_URL)
+def set_connection(factory):
+    def wrapper(func):
+        def inner(*args):
+            with connect(DATABASE_URL) as conn:
+                with conn.cursor(cursor_factory=factory) as cur:
+                    res = func(cur, *args)
+            conn.commit()
+            conn.close()
+            return res
+        return inner
+    return wrapper
 
 
-def add_url(url):
+@set_connection(extras.DictCursor)
+def add_url(cur, url):
     query = """INSERT INTO urls (name)
                 VALUES (%s)
                 RETURNING id;"""
-    with db_connect() as conn:
-        with conn.cursor() as cur:
-            cur.execute(query, (url,))
-            id = cur.fetchone()[0]
-    conn.commit()
-    conn.close()
+    cur.execute(query, (url,))
+    id = cur.fetchone()[0]
     return id
 
 
-def add_checked_url(url_data):
+@set_connection(extras.DictCursor)
+def add_checked_url(cur, url_data):
     query = """INSERT INTO url_checks
                 (url_id, status_code, h1, title, description)
                 VALUES (%s, %s, %s, %s, %s);"""
-    with db_connect() as conn:
-        with conn.cursor() as cur:
-            try:
-                cur.execute(query, list(url_data.values()))
-            except Exception as error:
-                print(error, '\n', type(error))
-    conn.commit()
-    conn.close()
+    cur.execute(query, list(url_data.values()))
 
 
-def exec_get_url(query, attr):
-    with db_connect() as conn:
-        with conn.cursor(cursor_factory=extras.RealDictCursor) as cur:
-            cur.execute(query, (attr,))
-            url_data = cur.fetchone()
-    conn.commit()
-    conn.close()
+@set_connection(extras.RealDictCursor)
+def get_url_by_name(cur, name):
+    query = """SELECT * FROM urls
+                WHERE name = %s;"""
+    cur.execute(query, (name,))
+    url_data = cur.fetchone()
     return url_data
 
 
-def get_url_by_name(name):
-    query = """SELECT * FROM urls
-                WHERE name = %s;"""
-    return exec_get_url(query, name)
-
-
-def get_url_by_id(id):
+@set_connection(extras.RealDictCursor)
+def get_url_by_id(cur, id):
     query = """SELECT * FROM urls
                 WHERE id = %s;"""
-    return exec_get_url(query, id)
+    cur.execute(query, (id,))
+    url_data = cur.fetchone()
+    return url_data
 
 
-def get_url_checks(id):
+@set_connection(extras.RealDictCursor)
+def get_url_checks(cur, id):
     query = """SELECT * FROM url_checks
                 WHERE url_id = %s
                 ORDER BY id DESC;"""
-    with db_connect() as conn:
-        with conn.cursor(cursor_factory=extras.RealDictCursor) as cur:
-            cur.execute(query, (id,))
-            check_data = cur.fetchall()
-    conn.commit()
-    conn.close()
+    cur.execute(query, (id,))
+    check_data = cur.fetchall()
     return check_data
 
 
-def get_checked_urls():
+@set_connection(extras.RealDictCursor)
+def get_checked_urls(cur):
     query = """SELECT DISTINCT ON (urls.id)
                         urls.id AS id,
                         name,
@@ -84,10 +78,6 @@ def get_checked_urls():
                 LEFT JOIN url_checks ON
                     urls.id = url_checks.url_id
                 ORDER BY id DESC"""
-    with db_connect() as conn:
-        with conn.cursor(cursor_factory=extras.RealDictCursor) as cur:
-            cur.execute(query)
-            checked_urls = cur.fetchall()
-    conn.commit()
-    conn.close()
+    cur.execute(query)
+    checked_urls = cur.fetchall()
     return checked_urls
